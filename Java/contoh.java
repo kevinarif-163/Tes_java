f2c7c2c8dbacd751830fa5a9f7b6b51a5502fc83
kevinarif-163 <mohammad_rudianto_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:13:24 +0700

    Add files via upload

---
+++ Java/contoh.java
@@ -0,0 +1,10 @@
+public class Tes {
+    Run|Debug
+    public static void main(string[] args) {
+        String nama = "kevin";
+        String kelas = "x rpl 5";
+        System.out.print("halo, nama saya" + nama + "dari kelas " + kelas)
+
+    }
+    
+}
